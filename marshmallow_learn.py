d473ddc1614d8050f479912f5a1644d5af5eeddd
kunpengku <fupeng_2005@126.com>
2017-06-17 18:09:40 +0800

    marshmallow

---
+++ marshmallow_learn.py
@@ -0,0 +1,25 @@
+from datetime import date
+from marshmallow import Schema, fields, pprint
+
+class ArtistSchema(Schema):
+    name = fields.Str()
+
+class AlbumSchema(Schema):
+    title = fields.Str()
+    release_date = fields.Date()
+    artist = fields.Nested(ArtistSchema)
+
+bowie = dict(name='David Bowie')
+album = dict(artist=bowie, title='Hunky Dory', release_date=date(1971, 12, 17))
+
+schema = AlbumSchema()
+result = schema.dump(album)
+#pprint(result.data, indent=2)
+# { 'artist': {'name': 'David Bowie'},
+#   'release_date': '1971-12-17',
+#   'title': 'Hunky Dory'}
+
+print album
+
+print result.data
+print type(result.data)
