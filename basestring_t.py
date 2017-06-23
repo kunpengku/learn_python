6261435a087c90e26838a68805fa83fd8e95c0ba
kunpengku <fupeng_2005@126.com>
2017-06-23 09:10:24 +0800

    basestring 判断一个字符串是否是 str 或者 unicode之一

---
+++ basestring_t.py
@@ -0,0 +1,21 @@
+
+
+s = 'a'
+
+
+print s
+print isinstance(s,basestring)
+print isinstance(s,(str, unicode))
+print isinstance(s,str)
+print isinstance(s,unicode)
+
+
+s2 = u'a'
+
+
+print s2
+print isinstance(s2,basestring)
+print isinstance(s2,(str, unicode))
+print isinstance(s2,str)
+print isinstance(s2,unicode)
+
