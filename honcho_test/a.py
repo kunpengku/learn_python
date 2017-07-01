2e736434f1e3e4258ab41aa59ce25b96f95cb60c
kunpengku <fupeng_2005@126.com>
2017-07-01 11:35:29 +0800

    honcho test

---
+++ honcho_test/a.py
@@ -0,0 +1,7 @@
+
+import time
+import os
+
+os.system("echo 123  > a")
+print "i'm in a"
+time.sleep(5)
---
+++ honcho_test/b.py
@@ -0,0 +1,8 @@
+
+import time
+import os
+
+os.system("echo b > b")
+print  'i am in b.py'
+
+time.sleep(3)
